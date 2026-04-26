b72382b981c93b082386bd71b9ffbd9311672df4
Sahana devi R A <sahanadevi2037@gmail.com>
2026-04-27 00:23:18 +0530

    Add files via upload

---
+++ average marks with functions.py
@@ -0,0 +1,15 @@
+print("average score:")
+
+def total(m1,m2,m3):
+    
+    t = m1 + m2 + m3
+    print("Total marks is: ",t)
+
+    avg = t/3
+    print("average marks is: ", avg)
+
+m1 = int(input("Enter m1: "))
+m2 = int(input("Enter m2: "))
+m3 = int(input("Enter m3: "))
+total(m1,m2,m3)
+    
