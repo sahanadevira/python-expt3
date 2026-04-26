e1e1c8407cf597a0f7357d82f3c89bcbf9b31c0f
Sahana devi R A <sahanadevi2037@gmail.com>
2026-04-27 00:22:28 +0530

    Add files via upload

---
+++ area of a rectangle.py
@@ -0,0 +1,12 @@
+print("area of a rectangle.")
+
+def area(a,b):
+    c = a*b
+    return c
+
+a = int(input("Enter the breadth : "))
+b = int(input("Enter the length  : "))
+
+value = area(a,b)
+
+print("the area of the rectangle is : ",value)
